d2a6eb2284311a51c28fceba474a75def848a4ad
rickrain <rickrain@hotmail.com>
2014-07-04 14:35:42 -0500

    Added PictureLoader.WebJob Project.

---
+++ PictureLoader.WebJob/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PictureLoader.WebJob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("{0} - Hello from PictureLoader.WebJob", DateTime.UtcNow);
+                // Sleep 60 seconds.
+                System.Threading.Thread.Sleep(60000);
+            }
+        }
+    }
+}
