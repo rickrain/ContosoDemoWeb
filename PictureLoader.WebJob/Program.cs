22fb4f47d076a67cada11e410b7bdd27a9da8c4c
rickrain <rickrain@hotmail.com>
2014-07-05 12:41:37 -0500

    Updated WebJob to trigger on Azure Storage Queue.

--- PictureLoader.WebJob/Program.cs
+++ PictureLoader.WebJob/Program.cs
@@ -1,20 +1,56 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.Storage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Microsoft.Azure.Jobs;
+using Models.Library;
+using Newtonsoft.Json;
+
 
 namespace PictureLoader.WebJob
 {
     class Program
     {
+        const string STORAGE_CONN_STR_KEY_NAME = "AzureJobsStorage";
+        const string PICTURE_LOADER_QUEUE_NAME = "picture-loader";
+
         static void Main(string[] args)
         {
-            while (true)
+            CloudStorageAccount storageAccount;
+            if (CloudStorageAccount.TryParse(
+                ConfigurationManager.ConnectionStrings[STORAGE_CONN_STR_KEY_NAME].ConnectionString, 
+                out storageAccount))
             {
-                Console.WriteLine("{0} - Hello from PictureLoader.WebJob", DateTime.UtcNow);
-                // Sleep 60 seconds.
-                System.Threading.Thread.Sleep(60000);
+                // Create the queue we're using to trigger on if it doesn't already exist.
+                CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+                CloudQueue queue = queueClient.GetQueueReference(PICTURE_LOADER_QUEUE_NAME.ToLower());
+                queue.CreateIfNotExists();
+            }
+            else
+            {
+                Console.WriteLine("Unable to initialize CloudStorageAccount.");
+            }
+
+            JobHost jobHost = new JobHost();
+            jobHost.RunAndBlock();
+        }
+
+        public static void ProcessQueueMsg([QueueTrigger(PICTURE_LOADER_QUEUE_NAME)] Picture picture)
+        {
+            if (Uri.IsWellFormedUriString(picture.PictureUrl, UriKind.Absolute))
+            {
+                Pictures picturesDB = new Pictures();
+                picturesDB.PictureEntities.Add(picture);
+                picturesDB.SaveChanges();
+            }
+            else
+            {
+                // Perform some compensating action here...
+                Console.WriteLine("Picture Url '{0}' is invalid.");
             }
         }
     }
